564cf3ec4173bbfbe3714fc884b0ce9d5cbc96cd
OA Wu <comdan66@gmail.com>
2017-03-10 15:52:51 +0800

    Fix code

--- application/models/LogText.php
+++ application/models/LogText.php
@@ -72,11 +72,14 @@ class LogText extends OaLineModel {
       foreach ($keywords as $keyword) {
         write_file (FCPATH . 'temp/input.json', "4 " . $keyword->pattern . " \n----------------------\n", FOPEN_READ_WRITE_CREATE);
 
-        if ($keys = LogText::regex ($keyword->pattern, $this->text))
+        if ($keys = LogText::regex ($keyword->pattern, $this->text)) {
+        write_file (FCPATH . 'temp/input.json', "5 " . count($keys) . " \n----------------------\n", FOPEN_READ_WRITE_CREATE);
+
           return array (
               'keys' => $keys,
               'keyword' => $keyword,
             );
+        }
       }
     }
     return array ();

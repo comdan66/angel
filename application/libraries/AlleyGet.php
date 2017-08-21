b1219a52c0336fe35641595dd69b8c0f8941398c
OA Wu <comdan66@gmail.com>
2017-08-21 17:28:38 +0800

    Fix code.

--- application/libraries/AlleyGet.php
+++ application/libraries/AlleyGet.php
@@ -80,7 +80,7 @@ class AlleyGet {
       return array (
           'title' => $item['productName'],
           'desc' => ($item['story']) ? $item['story'] : $item['address'],
-          'img' => $item['originImage'],
+          'img' => str_replace ('http://imagealley.friday.tw/', 'https://s3-ap-northeast-1.amazonaws.com/', $item['originImage']),
           'url' => $item['webSite'],
         );
     }, $data['items']), 0, 5);

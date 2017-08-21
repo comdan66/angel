45f55f176236e8752a6d0708221ed83c5b2a7fa2
OA Wu <comdan66@gmail.com>
2017-08-22 01:43:02 +0800

    Fix code.

--- application/controllers/api/send.php
+++ application/controllers/api/send.php
@@ -31,13 +31,13 @@ class Send extends Api_controller {
   }
 
   /**
-   * @api {get} /send/sticker/:packageId/:stickerId 景點內容
+   * @api {get} /send/sticker/:packageId/:stickerId 傳貼圖
    * @apiGroup Send
    *
    * @apiParam {String}      packageId    package ID
    * @apiParam {String}      stickerId    sticker ID
    *
-   * @apiParam {String}      user_id       User ID
+   * @apiParam {String}      user_id      接收者 User ID
    *
    * @apiSuccess {Boolean}   status       執行狀態
    *
@@ -57,21 +57,14 @@ class Send extends Api_controller {
    *     }
    */
   public function sticker ($packageId = 0, $stickerId = 0) {
-    if (!(($source = OAInput::get ('user_id')) && ($source = trim ($source)) && ($source = Source::find ('one', array ('select' => 'sid', 'conditions' => array ('sid = ? AND status = ?', $source, Source::STATUS_JOIN)))))) {
-      echo '<meta http-equiv="Content-type" content="text/html; charset=utf-8" /><pre>';
-      var_dump (Source::STATUS_JOIN);
-      exit ();
+    if (!(($source = OAInput::get ('user_id')) && ($source = trim ($source)) && ($source = Source::find ('one', array ('select' => 'sid', 'conditions' => array ('sid = ? AND status = ?', $source, Source::STATUS_JOIN))))))
       return $this->output_error_json ('使用者錯誤');
-    }
 
     if (!(($packageId = trim ($packageId)) && is_numeric ($packageId) && ($stickerId = trim ($stickerId)) && is_numeric ($stickerId)))
       return $this->output_error_json ('參數錯誤');
 
-    $channel_secret = Cfg::setting ('line', 'channel', 'secret');
-    $token = Cfg::setting ('line', 'channel', 'token');
-
-    $httpClient = new CurlHTTPClient ($token);
-    $bot = new LINEBot ($httpClient, ['channelSecret' => $channel_secret]);
+    $httpClient = new CurlHTTPClient (Cfg::setting ('line', 'channel', 'token'));
+    $bot = new LINEBot ($httpClient, ['channelSecret' => Cfg::setting ('line', 'channel', 'secret')]);
 
     $builder = new StickerMessageBuilder ((int)$packageId, (int)$stickerId);
     $response = $bot->pushMessage ($source->sid, $builder);
@@ -79,16 +72,53 @@ class Send extends Api_controller {
     return $this->output_json (array ('status' => true));
   }
 
+  /**
+   * @api {get} /send/location 傳定位
+   * @apiGroup Send
+   *
+   * @apiParam {String}      title       標題，最多 48 個字
+   * @apiParam {String}      address     地址，最多 48 個字
+   * @apiParam {String}      latitude    緯度
+   * @apiParam {String}      longitude   經度
+   *
+   * @apiParam {String}      user_id      接收者 User ID
+   *
+   * @apiSuccess {Boolean}   status       執行狀態
+   *
+   * @apiSuccessExample {json} Success Response:
+   *     HTTP/1.1 200 OK
+   *     {
+   *         "status": true
+   *     }
+   *
+   *
+   * @apiError   {String}    message     錯誤原因
+   *
+   * @apiErrorExample {json} Error-Response:
+   *     HTTP/1.1 405 Error
+   *     {
+   *         "message": "參數錯誤"
+   *     }
+   */
   public function location () {
-    $user_id = 'U4a37e32a1d11b3995d2bf299597e432f';
-    $channel_secret = Cfg::setting ('line', 'channel', 'secret');
-    $token = Cfg::setting ('line', 'channel', 'token');
+    if (!(($source = OAInput::get ('user_id')) && ($source = trim ($source)) && ($source = Source::find ('one', array ('select' => 'sid', 'conditions' => array ('sid = ? AND status = ?', $source, Source::STATUS_JOIN))))))
+      return $this->output_error_json ('使用者錯誤');
+    
+    $title = OAInput::get ('title');
+    $address = OAInput::get ('address');
+    $latitude = OAInput::get ('latitude');
+    $longitude = OAInput::get ('longitude');
+    
+    if (!(($title = trim ($title)) && ($title = mb_strimwidth ($title, 0, 48 * 2, '…','UTF-8')) && ($address = trim ($address)) && ($address = mb_strimwidth ($address, 0, 48 * 2, '…','UTF-8')) && is_numeric ($latitude = trim ($latitude)) && is_numeric ($longitude = trim ($longitude))))
+      return $this->output_error_json ('參數錯誤');
 
-    $httpClient = new CurlHTTPClient ($token);
-    $bot = new LINEBot ($httpClient, ['channelSecret' => $channel_secret]);
+    $httpClient = new CurlHTTPClient (Cfg::setting ('line', 'channel', 'token'));
+    $bot = new LINEBot ($httpClient, ['channelSecret' => Cfg::setting ('line', 'channel', 'secret')]);
 
-    $builder = new LocationMessageBuilder ('小夏，我在這裡，快來打我呀笨蛋～', '你右邊的後面的前面那位！', 25.075446, 121.573558);
+    $builder = new LocationMessageBuilder ($title, $address, $latitude, $longitude);
     $response = $bot->pushMessage ($user_id, $builder);
+
+    return $this->output_json (array ('status' => true));
   }
   public function img () {
     $user_id = 'U4a37e32a1d11b3995d2bf299597e432f';

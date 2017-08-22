7b8fc70c7c6514e565540bebe1ca9c41f6f9cd83
OA Wu <comdan66@gmail.com>
2017-08-22 17:31:05 +0800

    Fix code.

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url'] = ENVIRONMENT == 'production' ? 'https://angel.ioa.tw/say/' : '';
+$config['base_url'] = ENVIRONMENT == 'production' ? 'https://angel.ioa.tw/' : '';
 
 /*
 |--------------------------------------------------------------------------
